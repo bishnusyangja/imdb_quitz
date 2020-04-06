b6a48f2fde75505f7f228687c534dd8e99f4d495
Bishnu Bhattarai <bishnusyangja@gmail.com>
2020-04-06 21:28:52 +0545

    oscar content extracted

--- web_crawling.py
+++ web_crawling.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -25,8 +28,8 @@ class WebScrapping:
                 break
         return oscar_url, emmys_url
 
-    def fetch_oscar_content(self, url):
-        print('fetching oscar content.....')
+    def fetch_oscar_winner_urls(self, url):
+        print('fetching oscar winner urls .....')
         resp = requests.get(f'{self.ROOT_URL}{url}')
         bs = BeautifulSoup(resp.text, 'html.parser')
         anc = bs.find('a', attrs={'title': 'Winners'})
@@ -37,23 +40,66 @@ class WebScrapping:
             winners_url = ''
         return winners_url
 
-    def fetch_oscar_winner_urls(self, url):
-        print('fetching oscar winners.....')
-        resp = requests.get(f'{self.ROOT_URL}{url}')
-        bs = BeautifulSoup(resp.text, 'html.parser')
+    def fetch_oscar_winner_content(self, url):
 
-        for year_div in bs.find_all('div', attrs={'class': 'event-history-widget__years-row'}):
-            print(year_div)
-            anc = year_div.find('a')
-            print(anc.get_text(), anc.attrs.get('href'))
+        print('fetching oscar winners.....')
+        url = f'{self.ROOT_URL}{url}'
+        print('oscar nominies url ', url)
+        resp = requests.get(url)
+        bs = BeautifulSoup(resp.text, 'lxml')
+
+        js_scripts = bs.find_all('script')
+
+        for i_script in js_scripts:
+            check_string = 'IMDbReactWidgets.NomineesWidget.push'
+            text = i_script.get_text()
+            if check_string in text:
+                first_string = """{"nomineesWidgetModel":"""
+                second_string = "}]);"
+                try:
+                    first_idx = text.index(first_string)
+                    second_idx = text.index(second_string)
+                    content = text[first_idx: second_idx+1]
+                    return json.loads(content)
+                except Exception as exc:
+                    print("Exception", exc)
+        return {}
+
+    def extract_oscar_content(self, data):
+        # extracted_content = {}
+        awards = data['nomineesWidgetModel']['eventEditionSummary']['awards']
+        for award in awards:
+            award_name = award['awardName']
+            print('************\n')
+            if award_name == 'oscar':
+                for category in award['categories']:
+                    category_name = category['categoryName']
+                    print('.........')
+                    for nominees in category['nominations']:
+                        item = nominees['primaryNominees'][0]['name']
+                        is_winner = nominees['isWinner']
+                        print(award_name, category_name, item, is_winner)
+
+
+
+def data_for_oscar_award(obj, oscar_url):
+    # winners_url = obj.fetch_oscar_winner_urls(oscar_url)
+    winners_url = '/oscars/nominations/'
+    oscar_data = obj.fetch_oscar_winner_content(winners_url)
+    obj.extract_oscar_content(oscar_data)
+
+
+def data_for_emmys_award(obj, emmys_url):
+    winners_url = obj.fetch_emmys_winner_urls(emmys_url)
 
 
 def crawl_imdb_content():
     obj = WebScrapping()
     oscar_url, emmys_url = obj.fetch_oscar_emmy_url()
-    winners_url = obj.fetch_oscar_content(oscar_url)
-    obj.fetch_oscar_winner_urls(winners_url)
-    # print(winners_url)
+    data_for_oscar_award(obj, oscar_url)
+    data_for_emmys_award(obj, emmys_url)
+
+    # print('winners url ', winners_url)
     # print(oscar_url, emmys_url)
 
 
