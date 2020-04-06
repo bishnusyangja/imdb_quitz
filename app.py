9eede77bc260747c0a52a81a3768883f6c543e50
Bishnu Bhattarai <bishnusyangja@gmail.com>
2020-04-06 22:13:25 +0545

    fixed

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, make_response, jsonify
-from middleware import BaseMiddleware, LoginMiddleware
+from flask import Flask
+from middleware import LoginMiddleware
 
 
 def get_core_app():
     app = Flask(__name__)
-    app.wsgi_app = BaseMiddleware(app.wsgi_app)
     app.wsgi_app = LoginMiddleware(app.wsgi_app)
     return app
