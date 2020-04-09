02f6b5ea92af3a6070f881b43a34915273fb8e6b
Bishnu Bhattarai <bishnusyangja@gmail.com>
2020-04-09 07:51:50 +0545

    added run.sh

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
+import sys
+import os
+from os.path import dirname
+root_path = dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(root_path))
+
+
 from urls import get_flask_app
 
+app = get_flask_app()
 
-if __name__ == '__main__':
-   app = get_flask_app()
-   app.run(debug=True)
+# @app.after_request
+# def after_request(response):
+#   response.headers.add('Access-Control-Allow-Origin', '*')
+#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#   return response
+
+# if __name__ == '__main__':
+#    app.run(debug=True)
