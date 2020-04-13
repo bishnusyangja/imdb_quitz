3838eb67db005e2268e71101f6534066a9898d5d
Bishnu Bhattarai <bishnusyangja@gmail.com>
2020-04-13 22:11:04 +0545

    f

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -17,12 +17,12 @@ import Request from '../api'
 
     const [state, setState] = useState({
         data:[
-            {question: 'Which of the following is abc ? ', key_id: '', option1: 'a dfaf', option2: 'b dfadf', option3: 'c fdfadf', option4: 'd eeee'},
-            {question: 'Which of the following is dfdaf ? ', key_id: '', option1: 'a dfaf', optino2: 'b dfadf', option3: 'c ddd', option4: 'd dfafd'},
-            {question: 'Which of the following is aaaaabc ? ', key_id: '', option1: 'a rrr', option2: 'b 000', option3: 'c ert', option4: 'd rrr'},
-            {question: 'Which of the following is vvvv ? ', key_id: '', option1: 'a yyyy', option2: 'b ooo', option3: 'c fdfadf', option4: 'd gggg'},
-            {question: 'Which of the following is eeee ? ', key_id: '', option1: 'a tttt', option2: 'b ccc', option3: 'c fdfadf', option4: 'd dfafd'},
-            {question: 'Which of the following is rrrr ? ', key_id: '', option1: 'a fdaf', option2: 'b dfadf', option3: 'c fdfadf', option4: 'd rrr'}
+            {question: 'Which of the following is abc ? ', key_id: 'sdff', option1: 'a dfaf', option2: 'b dfadf', option3: 'c fdfadf', option4: 'd eeee'},
+            {question: 'Which of the following is dfdaf ? ', key_id: 'fdfsf', option1: 'a dfaf', optino2: 'b dfadf', option3: 'c ddd', option4: 'd dfafd'},
+            {question: 'Which of the following is aaaaabc ? ', key_id: 'ffdsf', option1: 'a rrr', option2: 'b 000', option3: 'c ert', option4: 'd rrr'},
+            {question: 'Which of the following is vvvv ? ', key_id: 'fdfas', option1: 'a yyyy', option2: 'b ooo', option3: 'c fdfadf', option4: 'd gggg'},
+            {question: 'Which of the following is eeee ? ', key_id: 'fadfsaf', option1: 'a tttt', option2: 'b ccc', option3: 'c fdfadf', option4: 'd dfafd'},
+            {question: 'Which of the following is rrrr ? ', key_id: 'dafdsaf', option1: 'a fdaf', option2: 'b dfadf', option3: 'c fdfadf', option4: 'd rrr'}
         ], start: true
     });
 
@@ -119,6 +119,8 @@ import Request from '../api'
     const onOptionChange = (key_id, value) => {
         ans[key_id] = value
         setAnswer(ans);
+        console.log(key_id);
+        console.log(ans)
     }
 
     const radioStyle = {
